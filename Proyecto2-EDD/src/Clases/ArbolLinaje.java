19f04f3fff9023b23adbb99e12f129dca8c8d31b
Alejandra Oporto <alejandra.oporto@correo.unimet.edu.ve>
2024-11-19 22:21:25 -0400

    Actualizacion ArbolLinaje

--- Proyecto2-EDD/src/Clases/ArbolLinaje.java
+++ Proyecto2-EDD/src/Clases/ArbolLinaje.java
@@ -4,10 +4,34 @@
  */
 package Clases;
 
+import EDD.Arbol;
+import EDD.HashTable;
 /**
  *
  * @author Alejandra Oporto
  */
 public class ArbolLinaje {
-    
+    private HashTable tablaLinaje;
+    private Arbol arbolLinaje;
+
+    public ArbolLinaje() {
+        this.tablaLinaje = new HashTable(100);
+        this.arbolLinaje = new Arbol();
+    }
+
+    public HashTable getTablaLinaje() {
+        return tablaLinaje;
+    }
+
+    public void setTablaLinaje(HashTable tablaLinaje) {
+        this.tablaLinaje = tablaLinaje;
+    }
+
+    public Arbol getArbolLinaje() {
+        return arbolLinaje;
+    }
+
+    public void setArbolLinaje(Arbol arbolLinaje) {
+        this.arbolLinaje = arbolLinaje;
+    }
 }
